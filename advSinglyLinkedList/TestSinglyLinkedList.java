e8e0423c4a16e5e1f6e83dcd5e108f0ba0483d90
indianmoody <gaurav.bishnoi@colorado.edu>
2016-05-25 15:53:20 -0600

    Adding Generic Singly Linked List with useful features

---
+++ advSinglyLinkedList/TestSinglyLinkedList.java
@@ -0,0 +1,162 @@
+// @author: Gaurav Bishnoi
+// Single LinkedList with advanced features, please refer "readme.txt".		
+		
+// Single Linked List
+public class TestSinglyLinkedList{
+
+     public static void main(String []args){
+        SinglyLinkedList lst = new SinglyLinkedList();
+        int x = 5;
+        int y = 7;
+        int z = 7;
+        lst.addFirst(x);
+        lst.addLast(y);
+        lst.addFirst(z);
+        System.out.println(lst.size());
+        /*System.out.println(lst.isEmpty());
+        lst.removeFirst();
+        System.out.println(lst.isEmpty());
+        System.out.println(lst.size());
+        lst.removeFirst();
+        System.out.println(lst.isEmpty());
+        System.out.println(lst.size());
+        lst.removeFirst();
+        System.out.println(lst.isEmpty());
+        System.out.println(lst.size());
+        lst.removeFirst();
+        System.out.println(lst.isEmpty());
+        System.out.println(lst.size());*/
+        lst.removeElement(y);
+        System.out.println(lst.size());
+     }
+}
+
+class SinglyLinkedList<E> {
+	
+	private static class Node<E> {
+		private E element;
+		private Node<E> next;
+		
+		public Node (E e, Node<E> n) {
+			element = e;
+			next = n;
+		}
+		
+		public E getElement() {
+			return element;
+		}
+		
+		public Node<E> getNext() {
+			return next;
+		}
+		
+		public void setNext(Node<E> e) {
+			next = e;
+		}
+	}
+	
+	private Node<E> head = null;
+	private Node<E> tail = null;
+	private int size = 0;
+	
+	public SinglyLinkedList() {}
+	
+	public E first() {
+		if (isEmpty())
+			return null;
+		return head.getElement();
+	}
+	
+	public E last() {
+		if (isEmpty())
+			return null;
+		return tail.getElement();
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		if (size == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	public void addFirst(E e) {
+		head = new Node<>(e, head);
+		if (isEmpty())
+			tail = head;
+		size++;
+	}
+	
+	public void addLast(E e) {
+		Node<E> newest = new Node<>(e, null);
+		if (isEmpty())
+			head = newest;
+		else
+			tail.setNext(newest);
+		tail = newest;
+		size++;
+	}
+	
+	public E removeFirst() {
+		if (isEmpty()) {
+			System.out.println("List is already Empty!");
+			return null;
+		}
+		E answer = head.getElement();	
+		head = head.getNext();
+		size--;
+		if (isEmpty()) {
+			tail = null;
+			System.out.println("Now, list is empty!");
+		}
+		return answer;
+	}
+	
+	public void removeElement(E e) {
+		int check = 0;
+		Node<E> n = head;
+		Node<E> m = head;
+		if (n.getElement() == e) {
+			head = head.getNext();
+			n = n.getNext();
+			size--;
+			if (isEmpty()) 
+				tail = null;
+			check++;
+		}
+			
+		while (n != null)
+		{
+			if (n.getElement() == e) {
+				m.setNext(n.getNext());
+				if (n.getNext() == null) {
+					tail = m;
+					n = n.getNext();
+				}
+				else {
+					n = n.getNext();
+				}
+				size--;
+				check++;
+			}
+			else {
+				m = n;
+				n = n.getNext();
+			}
+			
+		}
+		if (check == 0)
+			System.out.println("Specified element was not found in the list!");
+		else
+			System.out.println("Specified element is removed from list!");
+	}
+}
+
+
+// --------------------------------------------------------------------
+
+
---
+++ advSinglyLinkedList/readme.txt
@@ -0,0 +1,13 @@
+This is an impementation of Generic Single Linked List with useful methods. 
+
+Methods:
+size(): returns size of List
+first(): returns first element/ head of List without removing it
+last(): returns last element / tail of List without removing it
+isEmpty(): returns true if List is empty otherwise false
+addFirst(): appends specified element before the List
+addLast(): appends specified element after the List
+removeFirst(): removes first element/ head of the List
+removeElement(): removes specified element from the List(if present)
+
+
