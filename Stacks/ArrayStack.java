468c9744d088178c21643c35a83254e3eda095a7
indianmoody <gaurav.bishnoi@colorado.edu>
2016-05-31 13:01:17 -0600

    Adding Stack implemented with Array

---
+++ Stacks/ArrayStack.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class ArrayStack{
+
+     public static void main(String []args){
+        Stacks st = new Stacks();
+        int x = 5;
+        int y = 7;
+        int z = 11;
+        System.out.println(st.size());
+        st.push(x);
+        st.push(y);
+        st.push(z);
+        System.out.println(st.size());
+        int d = st.pop();
+        System.out.println(st.size());
+        st.pop();
+        st.pop();
+        System.out.println(st.size());
+        st.pop();
+        System.out.println("d = " + d);
+     }
+}
+
+class Stacks {
+    private int limit = 100;
+    private Integer[] arr = new Integer[limit];
+    private int size = -1;
+    
+    public boolean isEmpty() {
+        return (size == -1);
+    }
+    
+    public int size() {
+        return (size+1);
+    }
+    
+    public Integer peek() {
+        if (!isEmpty()) {
+            return arr[size];
+        }
+        else {
+            System.out.println("Stack is empty!");
+            return null;
+        }
+    }
+    
+    public void push (Integer data) throws IllegalStateException{
+        if (size() >= limit) throw new IllegalStateException("Stack is Full!");
+        arr[++size] = data;
+    }
+    
+    public Integer pop () {
+        if (!isEmpty()) {
+            Integer temp = arr[size];
+            arr[size] = null;
+            size--;
+            return temp;
+        }
+        else {
+            System.out.println("Stack is empty! Can't pop!");
+            return null;
+        }
+    }
+    
+}
---
+++ Stacks/readme.txt
@@ -0,0 +1,8 @@
+This is am implementation of Stack data structure using an array. 
+
+The implemented methods are:
+push(int x) : assigns x on top of Stack and increases the size
+pop() : returns and deletes data on top of Stack and decreases the size
+peek() : return data on top of Stack. doesn't delete the data
+size() : returns size of Stack
+isEmpty() : returns 'true' if Stack is empty otherwise 'false'
