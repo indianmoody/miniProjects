7291cc704880ace20887959cc618e5872eb88324
indianmoody <gaurav.bishnoi@colorado.edu>
2016-11-24 12:31:09 -0700

    gradle wrapper

--- quickGradle/src/main/java/hello/HelloWorld.java
+++ quickGradle/src/main/java/hello/HelloWorld.java
@@ -1,7 +1,12 @@
 package hello;
 
+import java.lang.Math;
+
 public class HelloWorld {
   public static void main(String[] args) {
+	  
+	System.out.println(Math.abs(-100));
+	  
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
